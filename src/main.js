bafcba126f08ff7e44b58524129b348650f7b83a
Cecilia Ribeiro <cecilia.rb@gmail.com>
2019-05-14 19:32:38 -0300

    Primeiro commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import plugin_teste from "plugin_teste_2";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(plugin_teste);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
