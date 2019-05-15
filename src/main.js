3b7666198e0d95f306fecf605a5be85d3e041a2f
Cecilia Ribeiro <cecilia.rb@gmail.com>
2019-05-15 00:34:56 -0300

    Adicionando plugin_teste_3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import plugin_teste from "plugin_teste_2";
+// import plugin_teste from "plugin_teste_2";
+import plugin_teste from "plugin_teste_3/dist/index.esm";
 
 Vue.config.productionTip = false;
 Vue.use(plugin_teste);
